ab710644daaa559898dd9a73a508b9e602c2c111
Lucas Rosa <lucasrof7@gmail.com>
2025-05-25 19:10:05 -0300

    Utilizando o throw para realizar a tratativa de exceções

--- Program.cs
+++ Program.cs
@@ -1,32 +1,6 @@
-﻿using ExemploExplorando.Models;
+﻿using aprendendo_CSharp.Models;
+using ExemploExplorando.Models;
 using System.Globalization;
 
-try
-{
 
-    string[] linhas = File.ReadAllLines("Arquivos/pasta1/arquivoLeitura.txt");
-
-    foreach (string linha in linhas)
-    {
-        Console.WriteLine(linha);
-    }
-}
-catch (FileNotFoundException ex)
-{
-    Console.WriteLine($"Ocorreu um erro na leitura do arquivo. Arquivo não encontrado. {ex.Message}");
-}
-catch (DirectoryNotFoundException ex)
-{
-    Console.WriteLine($"Ocorreu um erro na leitura do arquivo. O caminho da pasta não foi encontrado."
-    + ex.Message);
-}
-catch (Exception ex)
-{
-    Console.WriteLine($"Ocorreu uma excessão genérica. {ex.Message}");
-}
-finally
-{
-    Console.WriteLine("Chegou até aqui!");
-}
-
-
+new ExcecaoThrow().Metodo1();
--- obj/Debug/net9.0/ExemploExplorando.AssemblyInfo.cs
+++ obj/Debug/net9.0/ExemploExplorando.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4da1e4b02d1370f68f322bbf9e94a930a236b60")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69930df5e4bf8efa1fb7b07d28d001b47987946e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
